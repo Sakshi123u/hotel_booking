ebb0515b2e46b1fb45d15700c2601df18165a83a
Sakshi Pandharkar <pandharkarsakshi2005@gmail.com>
2025-06-20 22:51:11 +0530

    quick fix3

--- server/controllers/clerkWebhooks.js
+++ server/controllers/clerkWebhooks.js
@@ -22,9 +22,10 @@ const clerkWebhooks = async (req, res) => {
 
         const userData = {
             _id: data.id,
-            email: data.email_addresses[0].email_addresses,
+            email: data.email_addresses[0].email_address,
             username: data.first_name + " " + data.last_name,
             image: data.image_url,
+            recentSearchedCities: []
         }
 
         //switch cases for different events
